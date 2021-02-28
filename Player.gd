4d30d48c30aff66e5043a45e30ee45939fe80a31
Zenon <>
2021-02-28 19:48:05 +0100

    Added possibility to steer using UDP.

--- Player.gd
+++ Player.gd
@@ -2,16 +2,61 @@ extends KinematicBody
 
 class_name Player # why needed?
 
+var udpPort = 9999
+var udpThread
+
+var forward = 0
+var rotate_left = 0
+
 export var hspeed : float = 20.0
 export var rspeed : float = 2.0
 
 func _ready():
-	pass
+	print("Creating the UDP thread.")
+	udpThread = Thread.new()
+	udpThread.start(self, "receiver_thread", udpPort)
+#	pass
 
 func _physics_process(delta : float) -> void:
-	var dz : float = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
-	var da : float = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	var dz : float = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down") + forward
+	forward = 0
+	var da : float = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right") + rotate_left
+	rotate_left = 0
 	rotation.y += da*rspeed * delta
 	var direction : Vector3 = Vector3(0,0,1).rotated(Vector3(0,1,0), rotation.y)
 	var motion : Vector3 = direction * dz * hspeed * delta
-	move_and_collide(motion)
+	var _move_result = move_and_collide(motion)
+	#print("move resulted in "+str(move_result))
+
+func receiver_thread(port):
+	var done = false
+	var socket = PacketPeerUDP.new()
+	if(socket.listen(port, "127.0.0.1") != OK):
+		print("An error occurred listening on port "+str(port))
+		done = true;
+	else:
+		print("Listening on port " + str(port) + " on localhost")
+	while(done != true):
+		if(socket.get_available_packet_count() > 0):
+			var data = socket.get_packet().get_string_from_ascii()
+			if(data == "end"):
+				done = true
+			else:
+				print("Data received: " + data)
+				if(data == "forward"):
+					forward += 1
+				elif(data == "backward"):
+					forward -= 1
+				elif(data == "left"):
+					rotate_left += 5
+				elif(data == "right"):
+					rotate_left -= 5 
+	socket.close()
+	print("Exiting UDP Receiver")
+
+# not tested yet
+func send():
+	var socket = PacketPeerUDP.new()
+	socket.set_dest_address("127.0.0.1", udpPort)
+	socket.put_packet("Moin.".to_ascii())
+	print("UDP sending done.")
--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Warning 1: This is a playground. If it breaks your computer, I refrain from bein
 
 Warning 2: It is boring. I'm an absolute beginner, and this thing doesn't do anything interesting. I hope I'll be able to change that though.
 
-This is a [Godot](https://godotengine.org) project. To edit it, download Godot, and open it. To run it, just put the Godot engine into the same directory as the project.godot file, and start it.
+This is a [Godot](https://godotengine.org) project. To edit the project, download Godot, and open the project in Godot. To run the project, just put the Godot engine into the same directory as the project.godot file, and start the engine.
 
 ## Status
 
 - A floor, with a spot light to mark the center.
-- A "player": a cylinder with a nose (also a cylinder)
+- A "player": a cylinder with a red nose (also a cylinder)
 - A block that drops from the sky
 - Steer the player with arrow keys.
 - If you push the block off the floor, it'll vanish into the abyss. A new block will fall down under the spot light. A counter will be incremented.
 - Tab key brings the camera from somewhere to first person, shift-tab brings it to somewhere again.
+- Player.gd now contains some code that opens UDP port 9999, and waits for packages containing "forward", "backward", "left", or "right", so the player can be steered over the network.
